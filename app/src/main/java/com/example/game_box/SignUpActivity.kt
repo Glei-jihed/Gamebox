dd3da3a2a6dcc2be2ab65c2d9f164481a95ed7bb
Glei-jihed <glei.jihed@gmail.com>
2023-11-08 13:07:29 +0100

    V 1.3 CONNECTION BETWEEN SignUP Class and SignUp Layout

--- app/src/main/java/com/example/game_box/SignUpActivity.kt
+++ app/src/main/java/com/example/game_box/SignUpActivity.kt
@@ -1,12 +1,35 @@
 package com.example.game_box
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.game_box.databinding.ActivitySignUpBinding
 
-class SignUpActivity: AppCompatActivity() {
+class SignUpActivity: AppCompatActivity(), View.OnClickListener, View.OnFocusChangeListener, View.OnKeyListener {
+
+    private lateinit var mBinding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mBinding = ActivitySignUpBinding.inflate(LayoutInflater.from(this))
+        setContentView(mBinding.root)
+        mBinding.emailEt.onFocusChangeListener = this
+        mBinding.passET.onFocusChangeListener = this
+        mBinding.confirmPassEt.onFocusChangeListener = this
         setContentView(R.layout.activity_sign_up)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+        TODO("Not yet implemented")
     }
 
 }
