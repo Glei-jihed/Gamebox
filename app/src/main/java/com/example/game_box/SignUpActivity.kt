8191bd6261669ada9b7744222d90571c5b06d691
Glei-jihed <glei.jihed@gmail.com>
2023-11-08 13:33:52 +0100

    V 1.4 Creation of validation functions for amail password and confirm password

--- app/src/main/java/com/example/game_box/SignUpActivity.kt
+++ app/src/main/java/com/example/game_box/SignUpActivity.kt
@@ -1,11 +1,14 @@
 package com.example.game_box
 
 import android.os.Bundle
+import android.util.Patterns
+
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.game_box.databinding.ActivitySignUpBinding
+
 
 class SignUpActivity: AppCompatActivity(), View.OnClickListener, View.OnFocusChangeListener, View.OnKeyListener {
 
@@ -19,6 +22,93 @@ class SignUpActivity: AppCompatActivity(), View.OnClickListener, View.OnFocusCha
         mBinding.confirmPassEt.onFocusChangeListener = this
         setContentView(R.layout.activity_sign_up)
     }
+    private fun validateEmail():Boolean{
+        var errorMessage:String? = null
+        val value: String = mBinding.emailEt.text.toString()
+        if(value.isEmpty()){
+
+            errorMessage = "Email is required"
+
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(value).matches()){
+
+            errorMessage = "Email adress is invalid !"
+        }
+        if(errorMessage != null){
+
+            mBinding.emailLayout.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+
+        }
+        return errorMessage == null
+
+    }
+    private fun validatePassword():Boolean{
+        var errorMessage:String? = null
+        val value: String = mBinding.passET.text.toString()
+        if(value.isEmpty()){
+
+            errorMessage = "Password is required"
+
+        } else if (value.length < 6){
+
+            errorMessage = "Password must be 6 characters Long !"
+        }
+
+        if(errorMessage != null){
+
+            mBinding.passwordLayout.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+
+        }
+
+        return errorMessage == null
+    }
+
+    private fun validateConfirmpassword():Boolean {
+        var errorMessage:String? = null
+        val value: String = mBinding.confirmPassEt.text.toString()
+        if(value.isEmpty()){
+
+            errorMessage = " Confirm Password is required"
+
+        } else if (value.length < 6){
+
+            errorMessage = "Confirm Password must be 6 characters Long !"
+        }
+        if(errorMessage != null){
+
+            mBinding.confirmPasswordLayout.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+
+        }
+
+        return errorMessage == null
+    }
+
+    private fun validatePasswordAndConfirmPassword():Boolean{
+        var errorMessage: String? = null
+        val password: String? = mBinding.passET.text.toString()
+        val confirmPassword: String? = mBinding.confirmPassEt.text.toString()
+
+        if(password!=confirmPassword){
+            errorMessage = "Confirm password doesn't match with password"
+        }
+        if(errorMessage != null){
+
+            mBinding.confirmPasswordLayout.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+
+        }
+        return errorMessage ==null
+    }
 
     override fun onClick(v: View?) {
         TODO("Not yet implemented")
@@ -29,7 +119,7 @@ class SignUpActivity: AppCompatActivity(), View.OnClickListener, View.OnFocusCha
     }
 
     override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
-        TODO("Not yet implemented")
+        return false
     }
 
 }
