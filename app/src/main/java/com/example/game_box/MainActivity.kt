ac2f761c8cb475a2749773de377fc3c09c4484f4
Glei-jihed <glei.jihed@gmail.com>
2023-11-12 22:26:51 +0100

    V 2.0 Creation of the Login function and the MainActivity binding

--- app/src/main/java/com/example/game_box/MainActivity.kt
+++ app/src/main/java/com/example/game_box/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.game_box
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.game_box.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 }
--- app/src/main/java/com/example/game_box/SignInActivity.kt
+++ app/src/main/java/com/example/game_box/SignInActivity.kt
@@ -1,7 +1,8 @@
 package com.example.game_box
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.game_box.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -11,7 +12,43 @@ class SignInActivity : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivitySignInBinding.inflate(LayoutInflater.from(this))
+        mBinding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        mBinding.textView.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        mBinding.button.setOnClickListener {
+
+            val email = mBinding.emailEt.text.toString()
+            val password = mBinding.passET.text.toString()
+
+
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+
+                    firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            val intent = Intent(this,MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+
+            } else {
+                Toast.makeText(this, "Enter your data please !", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+
+
+
     }
 }
