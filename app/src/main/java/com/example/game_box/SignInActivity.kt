ec86197c18d0eb7a4767c85d82368fee2a494921
Glei-jihed <glei.jihed@gmail.com>
2023-11-12 22:41:30 +0100

    V 2.1 adding of the validation function of email and password to the SigninActivity

--- app/src/main/java/com/example/game_box/SignInActivity.kt
+++ app/src/main/java/com/example/game_box/SignInActivity.kt
@@ -2,12 +2,15 @@ package com.example.game_box
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.view.KeyEvent
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.game_box.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity(), View.OnFocusChangeListener, View.OnKeyListener {
     private lateinit var mBinding: ActivitySignInBinding
     private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,5 +53,57 @@ class SignInActivity : AppCompatActivity() {
 
 
 
+    }
+
+    override fun onFocusChange(v: View?, hasFocus: Boolean) {
+         fun validateEmail():Boolean{
+            var errorMessage:String? = null
+            val value: String = mBinding.emailEt.text.toString()
+            if(value.isEmpty()){
+
+                errorMessage = "Email is required"
+
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(value).matches()){
+
+                errorMessage = "Email adress is invalid !"
+            }
+            if(errorMessage != null){
+
+                mBinding.emailLayout.apply {
+                    isErrorEnabled = true
+                    error = errorMessage
+                }
+
+            }
+            return errorMessage == null
+
+        }
+        fun validatePassword():Boolean{
+            var errorMessage:String? = null
+            val value: String = mBinding.passET.text.toString()
+            if(value.isEmpty()){
+
+                errorMessage = "Password is required"
+
+            } else if (value.length < 6){
+
+                errorMessage = "Password must be 6 characters Long !"
+            }
+
+            if(errorMessage != null){
+
+                mBinding.passwordLayout.apply {
+                    isErrorEnabled = true
+                    error = errorMessage
+                }
+
+            }
+
+            return errorMessage == null
+        }
+    }
+
+    override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+        return false
     }
 }
