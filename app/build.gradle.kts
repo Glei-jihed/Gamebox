0e34f2f9d8d8b6fe23e8fc934e2c266d78b373d4
Glei-jihed <glei.jihed@gmail.com>
2023-11-13 22:33:18 +0100

    V 2.9  creation of the styled logout button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.21")
+
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
